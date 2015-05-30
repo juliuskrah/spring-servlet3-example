02558d1203be1a1686cca746ec0018062047352c
juliuskrah <juliuskrah@gmail.com>
2015-05-30 19:15:50 +0000

    major cleanup

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,37 @@
+/*
+ *Copyright 2015 Julius Krah
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
+package com.test;
+
+/**
+ * This app servers as the entry point into the application
+ * Collaborators:
+ * 	{@code Frederick Wordie}
+ *  {@code Felix Korsah}
+ *  {@code Nana Kwame}
+ *  
+ *  Other collaborators are invited to join by forking the repository
+ *  and making {@code pull request}
+ *  
+ * @author Julius Krah
+ *
+ */
+public class App {
+	public static void main(String.. args) {
+		
+	}
+	
+}
+
